3e559b0383151e51ded5bff06965bf256842ad35
oceanosis <dumluufuk@gmail.com>
2020-05-08 19:58:04 +0100

    cockrouch db in local

--- runCRDB.sh
+++ runCRDB.sh
@@ -5,46 +5,54 @@ BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$BASE/vars/setEnvVars"
 
-# Superuser settings
-CRDB_USER="docker"
-CRDB_DB="docker"
-CRDB_PASSWORD="docker"
-
 #CRDB_INITDB_ARGS=
 CRDBDATA="$BASE/crdbdata"
+INIT_SQL="$BASE/conf/crdb.sql"
 
+runsql() { ./cockroach sql --insecure -e "$1"; }
 
 # Create CRDB Cluster
 docker network create -d bridge roachnet || true
 
-docker stop roach1 roach2 roach3  || true && docker rm roach1 roach2 roach3|| true
+for node in roach1 roach2 roach3;
+do
+    docker exec -it $node ./cockroach quit --insecure || true
+    docker stop $node || true
+    docker rm $node || true
+done
 
-[ -d $CRDBDATA ] || mkdir -p "$CRDBDATA"
 
-docker run -d --name=roach1 --hostname=roach1 \
+if [ "$(ls -A $CRDBDATA)" ]; then
+    rm -rf $CRDBDATA && mkdir -p "$CRDBDATA"
+elif [ ! -d $CRDBDATA ]; then
+    mkdir -p "$CRDBDATA"
+fi
+
+docker run -d --name=roach1  --hostname=roach1 \
     --net=roachnet \
     -p 26257:26257 -p 8080:8080  \
     -v "$CRDBDATA/roach1":"/cockroach/cockroach-data" \
+	-v "$INIT_SQL":"/cockroach/cockroach-init.sql" \
     $CRDB_BUILD start \
     --insecure \
     --join=roach1,roach2,roach3 || false
 
-docker run -d --name=roach2 --hostname=roach2 \
+for node in roach2 roach3;
+do
+docker run -d --name=$node --hostname=$node \
     --net=roachnet \
-    -v "$CRDBDATA/roach2":"/cockroach/cockroach-data" \
+    -v "$CRDBDATA/$node":"/cockroach/cockroach-data" \
     $CRDB_BUILD start \
     --insecure \
     --join=roach1,roach2,roach3 || false
+done
 
-docker run -d --name=roach3 --hostname=roach3 \
-    --net=roachnet \
-    -v "$CRDBDATA/roach3":"/cockroach/cockroach-data" \
-    $CRDB_BUILD start \
-    --insecure \
-    --join=roach1,roach2,roach3 || false
+docker exec -it roach1 ./cockroach init --insecure || false
 
-docker exec -it roach1 ./cockroach init --insecure
+
+
+docker exec -it roach1 ./cockroach sql --insecure < /cockroach/cockroach-init.sql || false
 
 # To test connection;
-#  docker exec -it devdb sh -c 'exec psql  -U docker -d covid'
+#  docker exec -it roach1 ./cockroach sql --insecure
 exit 0;
